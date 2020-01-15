5e200219c9a89f37f309f3271d810839def00029
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-01-15 15:18:18 +0200

    edit main page

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,24 +24,24 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+    <div>
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <div className="background-img">
+          <div className="section-header">
+              <nav className="navbar-home">
+                  <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Work</a></li>
+                    <li><a href="#">Concat</a></li>
+                  </ul>
+              </nav>
+              <div className="intro-about-me">
+                  <h1>Yazan Al Monshed</h1>
+                  <p>this is me work hard in my self here</p>
+              </div>
+          </div>
       </div>
-    </>
+    </div>
   )
 }
 
